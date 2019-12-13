4f003f1ddc271d240dd4f574ca91f2c3e0ff7276
shane <shanecandoit@gmail.com>
2019-12-13 08:52:29 -0600

    first commit divby4 and divby100.

---
+++ leap.go
@@ -0,0 +1,22 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package leap should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package leap
+
+// IsLeapYear should have a comment documenting it.
+func IsLeapYear(year int) bool {
+	leap := false
+	// on every year that is evenly divisible by 4
+	if year%4 == 0 {
+		leap = true
+
+		// except every year that is evenly divisible by 100
+		if year%100 == 0 {
+			leap = false
+		}
+
+	}
+	return leap
+}
